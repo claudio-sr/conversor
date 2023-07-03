93c2cd306ec0b84b09d2f18995442c2f0ba7da32
claudio-sr <jclaudiosiqueira.js@gmail.com>
2023-07-03 12:23:03 -0300

    caixa de diálogo - Opões de Moedas

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-       JOptionPane.showInputDialog(null, "Escolha uma opção", "Menu",JOptionPane.PLAIN_MESSAGE, null, new Object[] {"Conversor de moeda", "Conversor de temperatura"}, "Conversor de moeda").toString();
+        Object[] conversor = {"Conversor de moeda", "Conversor de temperatura"};
+        JOptionPane.showInputDialog(null, "Escolha uma opção", "Menu", JOptionPane.PLAIN_MESSAGE, null, conversor, "Conversor de moeda").toString();
+        double valorMoeda = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira um valor:"));
+
+        Object[] moedaConverter = {"Reais -> Dólar", "Reais -> Euro", "Reais -> Libras Esterlinas", "Reais -> Peso argentino",
+        "Reais -> Peso Chileno", "Dólar -> Reais", "Euro -> Reais", "Libras Esterlinas -> Reais", "Peso argentino ->  Reais", "Peso Chileno -> Reais"};
+
+        JOptionPane.showInputDialog(null, "Escolha a moeda para a qual você deseja converter", "Menu", JOptionPane.PLAIN_MESSAGE, null, moedaConverter, "Conversor de moeda").toString();
+
     }
 }
